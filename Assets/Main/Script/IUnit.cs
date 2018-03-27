595108ae8bf321c6ccef751b81c01ef02b9c54f7
hakase0724 <hakase0274@gmail.com>
2018-03-27 18:57:07 +0900

    Unit
    ユニットの挙動を追加

---
+++ Assets/Main/Script/IUnit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ユニットが持つべきもの
+/// </summary>
+public interface IUnit
+{
+    //ユニットの体力
+    float UnitHp { get;set;}
+    //ユニットの動き
+    void Move();
+    //ユニットの攻撃
+    void Attack(float attack);
+    //ユニットがダメージを受ける
+    void Damage(float damage);
+}
--- Assets/Main/Script/PlayerUnit.cs
+++ Assets/Main/Script/PlayerUnit.cs
@@ -5,22 +5,55 @@ using Photon;
 using UniRx;
 using UniRx.Triggers;
 
-public class PlayerUnit : Photon.MonoBehaviour
+public class PlayerUnit : Photon.MonoBehaviour,IUnit
 {
-    private Main main;
+    public float UnitHp { get; set; }
+    private Rigidbody rb => GetComponent<Rigidbody>();
     [SerializeField]
     private Material[] playerColor = new Material[0];
     private void Start()
     {
-        if (photonView.isMine)
+        if (photonView.isMine) GetComponent<Renderer>().material = playerColor[0];
+        else if (!photonView.isMine)
         {
-            main = GameObject.FindGameObjectWithTag("Main").GetComponent<Main>();
-            GetComponent<Renderer>().material = playerColor[0];          
+            GetComponent<Renderer>().material = playerColor[1];
+            rb.isKinematic = true;
         }
-        else if(!photonView.isMine) GetComponent<Renderer>().material = playerColor[1];
-        this.OnMouseDownAsObservable()
-               .Where(_ => photonView.isMine)
-               .Subscribe(_ => Destroy(gameObject));
+        Move();
+    }
 
+    public void Move()
+    {
+        this.UpdateAsObservable()
+            .Subscribe(_ => rb.velocity = Vector3.forward);
+    }
+
+    public void Attack(float attack)
+    {
+
+    }
+
+    public void Damage(float damage)
+    {
+
+    }
+
+    private void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            //データの送信
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+            stream.SendNext(rb.velocity);
+          
+        }
+        else
+        {
+            //データの受信
+            transform.position = (Vector3)stream.ReceiveNext();
+            transform.rotation = (Quaternion)stream.ReceiveNext();
+            rb.velocity = (Vector3)stream.ReceiveNext();          
+        }
     }
 }
