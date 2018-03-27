3691208ecdf0df40ea5a0dcea8edaaedc6215203
hakase0724 <hakase0274@gmail.com>
2018-03-27 19:48:41 +0900

    同期テスト
    同期テスト

--- Assets/Main/Script/PlayerUnit.cs
+++ Assets/Main/Script/PlayerUnit.cs
@@ -37,23 +37,4 @@ public class PlayerUnit : Photon.MonoBehaviour,IUnit
     {
 
     }
-
-    private void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if (stream.isWriting)
-        {
-            //データの送信
-            stream.SendNext(transform.position);
-            stream.SendNext(transform.rotation);
-            stream.SendNext(rb.velocity);
-          
-        }
-        else
-        {
-            //データの受信
-            transform.position = (Vector3)stream.ReceiveNext();
-            transform.rotation = (Quaternion)stream.ReceiveNext();
-            rb.velocity = (Vector3)stream.ReceiveNext();          
-        }
-    }
 }
---
+++ Assets/Main/Script/SyncMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+
+public class SyncMove : Photon.MonoBehaviour
+{
+    private Rigidbody rb => GetComponent<Rigidbody>();
+    private void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            //データの送信
+            stream.SendNext(transform.position);
+            //stream.SendNext(transform.rotation);
+            stream.SendNext(rb.velocity);
+
+        }
+        else
+        {
+            //データの受信
+            transform.position = (Vector3)stream.ReceiveNext();
+            //transform.rotation = (Quaternion)stream.ReceiveNext();
+            rb.velocity = (Vector3)stream.ReceiveNext();
+        }
+    }
+
+}
