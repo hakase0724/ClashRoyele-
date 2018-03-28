7804a0e9bfd4f1673e1df5e8e774d5beff4f4e34
hakase0724 <hakase0274@gmail.com>
2018-03-28 11:12:32 +0900

    test

---
+++ Assets/Main/Script/InstantiateFiled.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+
+/// <summary>
+/// オブジェクトを生成する場所
+/// </summary>
+public class InstantiateFiled : Photon.MonoBehaviour
+{
+    [SerializeField]
+    private GameObject prefab;
+    private void OnClick()
+    {
+        if (!PhotonNetwork.inRoom) return;
+        PhotonNetwork.Instantiate(prefab.name, InputToEvent.inputHitPos, Quaternion.identity, 0);
+        photonView.RPC("RPCTest", PhotonTargets.All);
+    }
+
+    [PunRPC]
+    private void RPCTest()
+    {
+        Debug.Log("RPC" + PhotonNetwork.player.ID);
+    }
+	
+}
